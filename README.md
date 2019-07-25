3e437614925ed8e13eaa6b1fb77e270094fcfba1
Epasper <epasper@gmail.com>
2019-07-25 14:36:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-namutu-w81xw1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-namutu-w81xw1)
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    productNumber:0,
+    name: 'Ekspresowy Kurs Javy',
+    price: 7.99,
+    description: 'Bez stresu i z darmową kawą'
+  },
+  {
+    productNumber:1,
+    name: 'Darmowe ciastka i miła atmosfera',
+    price: 6.99,
+    description: 'Spotkaj się już dziś z gronem przyjaciół wokół IntelliJa'
+  },
+  {
+    productNumber:2,
+    name: 'Od zmiennych aż do tworzenia internetowych sklepików',
+    price: 2.99,
+    description: 'A wszystko za frytę'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
